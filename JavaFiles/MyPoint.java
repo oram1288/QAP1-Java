edaeecb4a21cdd8041bd895a4f5cf2949657ef8a
oram1288 <cody.oram@keyin.com>
2024-05-30 20:52:15 -0230

    final commit

--- JavaFiles/MyPoint.java
+++ JavaFiles/MyPoint.java
@@ -1,41 +1,61 @@
 package JavaFiles;
 
 public class MyPoint {
-    private int x;
-    private int y;
-
-    // Constructor
-    public MyPoint(int x, int y) {
+    private int x=0;
+    private int y=0;
+    //no argument constructor
+    public MyPoint()
+    {
+        this.x = 0;
+        this.y = 0;
+    }
+    //parameterized constructor
+    public MyPoint(int x, int y)
+    {
         this.x = x;
         this.y = y;
     }
-
-    // Getters and Setters
-    public int getX() {
-        return x;
-    }
-
+   //Setter & Getter Method
     public void setX(int x) {
         this.x = x;
     }
-
-    public int getY() {
-        return y;
+    public int getX() {
+        return x;
     }
-
     public void setY(int y) {
         this.y = y;
     }
-
-    // Method to return the distance between this point and another point
-    public double distance(MyPoint another) {
-        int xDiff = this.x - another.x;
-        int yDiff = this.y - another.y;
-        return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+    public int getY() {
+        return y;
     }
+    public void setXY(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public String toString()
+    {
+        return ("(" + this.x + "," +  this.y + ")");
+    }
+    public double distance(int x, int y)
+    {
+        int xDiff = this.x-x;
+        int yDiff = this.y-y;
 
-    // @Override
-    public String toString() {
-        return "(" + x + "," + y + ")";
+        return (Math.sqrt((xDiff*xDiff) + (yDiff*yDiff)));
+    }
+    public double distance(MyPoint p)
+    {
+        int xDiff = this.x-p.x;
+        int yDiff = this.y-p.y;
+
+        return (Math.sqrt((xDiff*xDiff) + (yDiff*yDiff)));
+    }
+    public double distance()
+    {
+        int xDiff = this.x-0;
+        int yDiff = this.y-0;
+
+        return (Math.sqrt((xDiff*xDiff) + (yDiff*yDiff)));
     }
 }
