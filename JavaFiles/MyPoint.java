9d52e02dd1ee89d61ff8809fb1c868bb099103c8
oram1288 <cody.oram@keyin.com>
2024-05-29 21:35:50 -0230

    first commit

--- JavaFiles/MyPoint.java
+++ JavaFiles/MyPoint.java
@@ -1,5 +1,41 @@
 package JavaFiles;
 
 public class MyPoint {
-    
+    private int x;
+    private int y;
+
+    // Constructor
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getters and Setters
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    // Method to return the distance between this point and another point
+    public double distance(MyPoint another) {
+        int xDiff = this.x - another.x;
+        int yDiff = this.y - another.y;
+        return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+    }
+
+    // @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
 }
